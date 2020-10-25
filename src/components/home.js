50a0b2440b893be3e74058a18003ade15ba17b95
ARedondoVilla <alexredondovilla@gmail.com>
2020-10-25 19:34:10 +0000

    initial commit

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function(props) {
-    return (
+    const [task, setTask] = useState("")
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+       // console.log(list);
+       console.log(task);
+        
+    });
+
+    function newTask(event) {
+        setTask(event.target.value);  
+    }
+
+    function pressEnter(event) {
+        // console.log(event.key);
+        if (task == "") {
+            return
+        }
+
+        if (event.key === "Enter") {
+            setList(list.concat(task));
+            setTask("");
+        }
+    }
+
+     return (
         <main className="page">
-            <h1>Home page</h1>
+            <h1>To Do List</h1>
+
+            <input type="text" onChange={newTask} onKeyPress={pressEnter} value={task}/>
+
+            <ul>
+                {list.map((value, index) => {return <li key={index}>{value}</li>})}
+            </ul>
+
         </main>
     )
 }
